32fd57c5f82d17606db3aa592f61507b8f20e2a6
Matt Fleming <matthewfleming8@gmail.com>
2020-03-26 00:44:20 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+  if speed < 40
+    return true
+  elsif speed > 60
+    return true
+  else
+    return false
 end
-
-
+unsafe?(50)
+end
 
 def not_safe?(speed)
-	
+ speed < 40 || speed > 60 ? true : false
 end
-	
+
 
 
